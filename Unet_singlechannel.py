16898261b20e400994f71c5f289a5fd7578803b8
walterwht <64456074+walterwht@users.noreply.github.com>
2020-06-15 12:21:49 +0800

    Update Unet_singlechannel.py

--- Unet_singlechannel.py
+++ Unet_singlechannel.py
@@ -72,18 +72,16 @@ class UNet(nn.Module):
         super(UNet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
-        self.bilinear = bilinear
 
         self.inc = DoubleConv(n_channels, 64)
         self.down1 = Down(64, 128)
         self.down2 = Down(128, 256)
         self.down3 = Down(256, 512)
-        factor = 2 if bilinear else 1
-        self.down4 = Down(512, 1024 // factor)
-        self.up1 = Up(1024, 512 // factor, bilinear)
-        self.up2 = Up(512, 256 // factor, bilinear)
-        self.up3 = Up(256, 128 // factor, bilinear)
-        self.up4 = Up(128, 64, bilinear)
+        self.down4 = Down(512, 1024 // 2)
+        self.up1 = Up(1024, 512 // 2)
+        self.up2 = Up(512, 256 // 2)
+        self.up3 = Up(256, 128 // 2)
+        self.up4 = Up(128, 64)
         self.outc = OutConv(64, n_classes)
 
     def forward(self, x):
@@ -99,4 +97,3 @@ class UNet(nn.Module):
         logits = self.outc(x)
         return logits
 
-a
