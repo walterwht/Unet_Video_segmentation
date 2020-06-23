2b9de5c6b2ee0f39314755cb755a81179359d02d
walterwht <64456074+walterwht@users.noreply.github.com>
2020-06-24 01:17:56 +0800

    Update Unet_singlechannel.py

--- Unet_singlechannel.py
+++ Unet_singlechannel.py
@@ -13,34 +13,32 @@ from PIL import Image
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class DoubleConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, padding=0, kernel_size=1, stride=1):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
         super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, padding=padding, kernel_size=kernel_size, stride=stride)
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU()
-        self.conv2 = nn.Conv2d(out_channels, out_channels, padding=padding, kernel_size=kernel_size, stride=stride)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.relu2 = nn.ReLU()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
 
     def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x = self.relu(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu2(x)
-        return x
+        return self.double_conv(x)
 
 class Down(nn.Module):
-  def __init__(self, in_channels, out_channels):
-    super().__init__()
-    self.maxpool=nn.MaxPool2d(2)
-    self.conv=DoubleConvBlock(in_channels, out_channels)
-  
-  def forward(self,x):
-    x=self.maxpool(x)
-    x=self.conv(x)
-    return x
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConvBlock(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
  
 class Up(nn.Module):
   def __init__(self,in_channels,out_channels):
