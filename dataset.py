8f643498e5e81b138a6e6f14459d6321ee0b3bd6
walterwht <64456074+walterwht@users.noreply.github.com>
2020-07-10 03:19:36 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -55,6 +55,8 @@ def transformdata(image, mask):
 
     # Transform to tensor
     image = TF.to_tensor(image)
+    image = normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])(image)
     #newMasks[0] = 1-newMasks[1:][0]
     
     
